a8e57da090fb683210ab2eba816007eb81854577
Bo Fang <fangboBarry@outlook.com>
2026-03-04 15:28:28 +0000

    Add merge sort implementation

---
+++ merge_sort.py
@@ -0,0 +1,11 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr)//2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return sorted(left + right)
+
+print(merge_sort([5,3,8,1]))
